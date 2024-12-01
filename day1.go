b2c6da5224587937958babbb5e127168d85f28d8
Simón Oroño <simonorono@protonmail.com>
2024-11-30 20:57:43 -0400

    Create minimal struct to execute days and skeleton for day 1

---
+++ day1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "embed"
+)
+
+type Day1 struct{}
+
+//go:embed input/day1.txt
+var Day1Input string
+
+func (day Day1) part1() interface{} {
+	return nil
+}
+
+func (day Day1) part2() interface{} {
+	return nil
+}
--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+type Day interface {
+	part1() interface{}
+	part2() interface{}
+}
+
 func main() {
-	fmt.Println("Hello")
+	days := []Day{}
+
+	for idx, day := range days {
+		fmt.Println("Day", idx+1)
+		fmt.Println("  Part 1:", day.part1())
+		fmt.Println("  Part 2:", day.part2())
+	}
 }
